ee13a169db15e50e67e2b5d0b940f06fd686dcf7
Kishan Gupta <kishangupta4514@gmail.com>
2024-04-26 13:12:59 +0530

    done

--- program2.js
+++ program2.js
@@ -2,6 +2,24 @@ function longestSubstring(s) {
 
 
     // Implementation of longestSubstring function
+
+    var n = s.length; 
+    var mx = 0; 
+    
+    for(var i = 0; i < n; i++) { 
+        var visited = new Array(256); 
+        for(var j = i; j < n; j++) { 
+            
+            if (visited[s.charAt(j)] == true) break;
+            else { 
+                mx = Math.max(mx, j - i + 1); 
+                visited[s.charAt(j)] = true; 
+            } 
+        } 
+
+        visited[s.charAt(i)] = false; 
+    } 
+    return mx; 
 }
 
 module.exports = { longestSubstring };
